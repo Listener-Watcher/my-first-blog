476151f577ccdd85ee1a51ab28e814828ccba3f5
Listener-Watcher <philking492@gmail.com>
2018-02-06 01:12:22 -0800

    Blog test

---
+++ app_blog/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+from django.utils import timezone
+
+#models.Model means that the Post is a Django Model, so Django knows that it should be saved in the database
+class Post(models.Model):
+	author = models.ForeignKey('auth.User',on_delete=models.CASCADE)# a link to another model
+	title = models.CharField(max_length=200)						# define text with a limited number of char
+	text = models.TextField()										# for long text without a limit
+	created_date = models.DateTimeField(default=timezone.now)		# date and time(might change based on version)
+	published_date = models.DateTimeField(blank=True,null=True)		# date and time again
+	
+	#publish method
+	def publish(self):
+		self.published_date = timezone.now()
+		self.save()
+	#return a string
+	def __str__(self):
+		return self.title
